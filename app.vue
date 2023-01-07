a07edcde84da3c9a079ac45c6a5200a69677ab20
Mazaev Andrey <mazaev2000@gmail.com>
2023-01-07 15:09:29 +0300

    init commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mx-auto flex min-h-screen flex-col">
+    <Header />
+    <div class="mt-20 flex flex-1 lg:mt-28">
+      <NuxtPage />
+    </div>
+    <Footer />
+  </div>
+</template>
