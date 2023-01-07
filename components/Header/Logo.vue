29927638de5b8147caf7dbbebd0508a29acad983
Mazaev Andrey <mazaev2000@gmail.com>
2023-01-07 15:54:37 +0300

    feat: header

---
+++ components/Header/Logo.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class=" text-4xl">Logo</h1>
+  </div>
+</template>
---
+++ components/Header/index.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header class="fixed top-0 left-0 right-0 z-50 bg-white shadow h-16 ">
+    <div class="container mx-auto">
+      <div class="flex items-center justify-between">
+        <HeaderLogo />
+        <ul class="flex gap-10 text-lg h-16 items-center">
+          <li>
+            <nuxt-link to="/">Главная</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="price">Услуги и цены</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="contact">Контакты</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
