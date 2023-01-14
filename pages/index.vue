31a41b6da27aa7572e6afe461a3332e935098342
Mazaev Andrey <mazaev2000@gmail.com>
2023-01-14 21:01:00 +0300

    feat: home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,48 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const categories = [
+  {
+    name: "Индивидуальные съёмки на улице",
+    slug: "/street",
+    src: "/street/Anna/1_fjmpnk.jpg",
+  },
+  {
+    name: "Уличные фотографии",
+    slug: "/city",
+    src: "/city/1_kygyms.jpg",
+  },
+  {
+    name: "Индивидуальные съёмки в студии",
+    slug: "/studio",
+    src: "studio/Kate/1_r2b5ok.jpg",
+  },
+];
+</script>
 
 <template>
   <div class="container mx-auto">
-    Page: Home Page
+    <div class="grid grid-cols-3 gap-10 text-center">
+      <nuxt-link
+        class="overflow-hidden"
+        v-for="item in categories"
+        :key="item.src"
+        :to="item.slug"
+      >
+        <div class="overflow-hidden rounded-xl shadow-xl">
+          <nuxt-img
+            preload
+            loading="lazy"
+            class="transition-all duration-300 hover:scale-110"
+            provider="cloudinary"
+            :src="item.src"
+            fit="fill"
+            :modifiers="{
+              fit: 'cover',
+              format: 'webp',
+            }"
+          />
+        </div>
+        <h2 class="mt-4 text-xl tracking-wide">{{ item.name }}</h2>
+      </nuxt-link>
+    </div>
   </div>
 </template>
