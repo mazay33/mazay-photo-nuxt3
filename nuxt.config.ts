0ac75ff662071782d6186686b3e4b3da9220bdc4
Mazaev Andrey <mazaev2000@gmail.com>
2023-01-14 21:00:41 +0300

    feat: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules:[
+    '@pinia/nuxt',
+    '@nuxt/image-edge',
+  ],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -7,4 +11,9 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  image: {
+    cloudinary: {
+        baseURL: 'https://res.cloudinary.com/dnvvkg9ou/image/upload/v1673686895/mazay33'
+    },
+  }
 })
